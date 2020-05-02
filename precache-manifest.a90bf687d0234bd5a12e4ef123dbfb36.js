f168dc3e0cd1bc5f59dabd2d1161ad346fd1bf73
Madison Leopold <leopold.madison@gmail.com>
2020-05-02 13:21:05 -0400

    Updates

---
+++ precache-manifest.a90bf687d0234bd5a12e4ef123dbfb36.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe0c3b6b9c0c63b0773c191058b21a1e",
+    "url": "/react-ecommerce/index.html"
+  },
+  {
+    "revision": "56c30ccbe87d3dd556fa",
+    "url": "/react-ecommerce/static/css/2.2d5d2296.chunk.css"
+  },
+  {
+    "revision": "c9e92f1aa5b8233397c2",
+    "url": "/react-ecommerce/static/css/main.70a97254.chunk.css"
+  },
+  {
+    "revision": "56c30ccbe87d3dd556fa",
+    "url": "/react-ecommerce/static/js/2.66add71e.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/react-ecommerce/static/js/2.66add71e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9e92f1aa5b8233397c2",
+    "url": "/react-ecommerce/static/js/main.2e3a8604.chunk.js"
+  },
+  {
+    "revision": "080b56ed1c65cfc6425c",
+    "url": "/react-ecommerce/static/js/runtime-main.e6e77293.js"
+  },
+  {
+    "revision": "e209a4b09c174fbec7c9356089a286f8",
+    "url": "/react-ecommerce/static/media/ebb2.e209a4b0.svg"
+  },
+  {
+    "revision": "2bd8422568e7bff4ac9ddc35d628dc2c",
+    "url": "/react-ecommerce/static/media/luxtreme.2bd84225.svg"
+  },
+  {
+    "revision": "a6dbe5fff5038d3b4ff973608d781181",
+    "url": "/react-ecommerce/static/media/save.a6dbe5ff.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react-ecommerce/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react-ecommerce/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react-ecommerce/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react-ecommerce/static/media/slick.f97e3bbf.svg"
+  }
+]);
